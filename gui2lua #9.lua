6201f909b876b708ed93c1153e027af3de39d392
Coolman19181 <168024766+Coolman19181@users.noreply.github.com>
2024-05-27 18:34:15 -0400

    Add files via upload

---
+++ gui2lua #9.lua
@@ -0,0 +1,91 @@
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+local Nameless = Instance.new("TextButton")
+local fedoraadmin = Instance.new("TextButton")
+local Energizegui = Instance.new("TextButton")
+local SPAMCHAT = Instance.new("TextButton")
+local _4nn1gui = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ResetOnSpawn=false
+
+ImageLabel.Parent = ScreenGui
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.159624413, 0, 0.141975313, 0)
+ImageLabel.Size = UDim2.new(0, 250, 0, 256)
+ImageLabel.Image = "rbxassetid://17638519238"
+ImageLabel.ImageTransparency = 0.400
+
+Nameless.Name = "Nameless"
+Nameless.Parent = ImageLabel
+Nameless.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Nameless.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Nameless.BorderSizePixel = 3
+Nameless.Position = UDim2.new(0.0520000011, 0, 0.26953125, 0)
+Nameless.Size = UDim2.new(0, 103, 0, 40)
+Nameless.Font = Enum.Font.SourceSans
+Nameless.Text = "Nameless Admin"
+Nameless.TextColor3 = Color3.fromRGB(0, 0, 0)
+Nameless.TextSize = 14.000
+Nameless.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FD2Team/Nameless-Admin-No-Byfron-Kick/main/Source",true))()
+end)
+fedoraadmin.Name = "fedora admin"
+fedoraadmin.Parent = ImageLabel
+fedoraadmin.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+fedoraadmin.BorderColor3 = Color3.fromRGB(0, 0, 0)
+fedoraadmin.BorderSizePixel = 3
+fedoraadmin.Position = UDim2.new(0.541596174, 0, 0.268325627, 0)
+fedoraadmin.Size = UDim2.new(0, 103, 0, 40)
+fedoraadmin.Font = Enum.Font.SourceSans
+fedoraadmin.Text = "Fedora Admin"
+fedoraadmin.TextColor3 = Color3.fromRGB(0, 0, 0)
+fedoraadmin.TextSize = 14.000
+fedoraadmin.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/alexx1212/fedoratoomutch/main/toomucth'))()
+end)
+Energizegui.Name = "Energize gui"
+Energizegui.Parent = ImageLabel
+Energizegui.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Energizegui.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Energizegui.BorderSizePixel = 3
+Energizegui.Position = UDim2.new(0.0508826301, 0, 0.486159325, 0)
+Energizegui.Size = UDim2.new(0, 103, 0, 38)
+Energizegui.Font = Enum.Font.SourceSans
+Energizegui.Text = "Energize Gui"
+Energizegui.TextColor3 = Color3.fromRGB(0, 0, 0)
+Energizegui.TextSize = 14.000
+Energizegui.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://pastebin.com/raw/0MLPL32f'))()
+end)
+SPAMCHAT.Name = "SPAM CHAT"
+SPAMCHAT.Parent = ImageLabel
+SPAMCHAT.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SPAMCHAT.BorderColor3 = Color3.fromRGB(0, 0, 0)
+SPAMCHAT.BorderSizePixel = 3
+SPAMCHAT.Position = UDim2.new(0.541596174, 0, 0.478941619, 0)
+SPAMCHAT.Size = UDim2.new(0, 102, 0, 40)
+SPAMCHAT.Font = Enum.Font.SourceSans
+SPAMCHAT.Text = "print ig"
+SPAMCHAT.TextColor3 = Color3.fromRGB(0, 0, 0)
+SPAMCHAT.TextSize = 14.000
+SPAMCHAT.MouseButton1Down:connect(function()
+	print("bopz41")
+end)
+_4nn1gui.Name = "4nn1 gui"
+_4nn1gui.Parent = ImageLabel
+_4nn1gui.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_4nn1gui.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_4nn1gui.BorderSizePixel = 0
+_4nn1gui.Size = UDim2.new(0, 248, 0, 56)
+_4nn1gui.Font = Enum.Font.SourceSans
+_4nn1gui.Text = "4nn1 gui"
+_4nn1gui.TextColor3 = Color3.fromRGB(0, 0, 0)
+_4nn1gui.TextSize = 14.000
